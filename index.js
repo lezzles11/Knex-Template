8ddaec6b9cc42dcdd1d51c5713b58e07a58002f6
lesleyc@bu.edu <lesleyc@bu.edu>
2020-07-30 09:45:06 +0800

    Home and form page works!

--- index.js
+++ index.js
@@ -7,15 +7,34 @@
 // importing the package
 const express = require("express");
 
+const bodyParser = require("body-parser");
+const hbs = require("express-handlebars");
 // using the package
 const app = express();
 
+// Configure for HBS
+app.engine(
+  "hbs",
+  hbs({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.set("view engine", "hbs");
+// configuration to actually use the package
+
+//
+app.use(bodyParser.urlencoded({ extended: false }));
+// this will make sure the response comes, it will be in json (string format)
+app.use(bodyParser.json());
+
 // creating a route
 app.get("/", (request, response) => {
-  response.send("Hello! This works!");
+  response.render("home");
 });
-app.get("/package", (request, response) => {
-  response.send("Hello! This is the package route!");
+app.get("/form", (request, response) => {
+  response.render("form");
 });
 
 // at the end of your application, you just connect to the port
